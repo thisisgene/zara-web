480b7ada686e918ff59ac45c882809b387ae0eac
thisisgene <emdo2000+genegit@gmail.com>
2018-12-21 15:09:16 +0100

    Removed icon from .gitignore

--- client/src/components/user/dashboard/IconObject/IconObject.js
+++ client/src/components/user/dashboard/IconObject/IconObject.js
@@ -3,15 +3,26 @@ import React, { Component } from 'react'
 import ActIcon from '../../common/img/act.png'
 import LockIcon from '../../common/img/lock.svg'
 
+// import fbLogo from '../../common/social media/facebook.svg'
+// import instaLogo from '../../common/social media/instagram.svg'
+// import newsletterLogo from '../../common/social media/newsletter.svg'
+// import twitterLogo from '../../common/social media/twitter.svg'
+// import ytLogo from '../../common/social media/youtube.svg'
 import ArrowIcon from '../../common/assets/icon/arrow/tertiary.svg'
-
-import search from '../../common/assets/button/search.svg'
 
 import fbLogo from '../../common/assets/icon/social/facebook.svg'
 import instaLogo from '../../common/assets/icon/social/instagram.svg'
 import newsletterLogo from '../../common/assets/icon/social/email.svg'
 import twitterLogo from '../../common/assets/icon/social/twitter.svg'
-import ytLogo from '../../common/assets/icon/social/twitter.svg'
+import ytLogo from '../../common/assets/icon/social/youtube.svg'
+
+// const ArrowIcon = '/assets/img/assets/icon/arrow/tertiary.svg'
+
+// const fbLogo = '/assets/img/assets/icon/social/facebook.svg'
+// const instaLogo = '/assets/img/assets/icon/social/instagram.svg'
+// const newsletterLogo = '/assets/img/assets/icon/social/email.svg'
+// const twitterLogo = '/assets/img/assets/icon/social/twitter.svg'
+// const ytLogo = '/assets/img/assets/icon/social/youtube.svg'
 
 class IconObject extends Component {
   render() {
@@ -21,7 +32,6 @@ class IconObject extends Component {
         act: ActIcon,
         listArrow: ArrowIcon,
         LockIcon,
-        search,
         fbLogo,
         instaLogo,
         twitterLogo,
