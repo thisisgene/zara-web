639dc17ff6045f11e4a1aeaf10da610c19b444b9
thisisgene <emdo2000+genegit@gmail.com>
2019-05-13 19:53:46 +0200

    Styled training details some

--- client/src/components/admin/pages/Training/Calendar/TrainingDetail/TrainingDetail.js
+++ client/src/components/admin/pages/Training/Calendar/TrainingDetail/TrainingDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Moment from 'react-moment'
 
+import cx from 'classnames'
 import styles from './TrainingDetail.module.sass'
 
 export default class TrainingDetail extends Component {
@@ -19,56 +20,146 @@ export default class TrainingDetail extends Component {
       )
 
     return (
-      <div className={styles['training-detail']}>
+      <div className={styles['training-detail-container']}>
         {training && (
-          <div className={styles['training-detail--container']}>
-            <div>
-              <h1>{training[0].title}</h1>
-            </div>
-            <div>
-              <p>{training[0].location.title}</p>
-              <p>{training[0].location.address1}</p>
-            </div>
-            <div>
-              <Moment format={'DD. MMMM YYYY'} locale={'de-DE'}>
-                {training[0].date}
-              </Moment>
-              <div>{training[0].timeFrom}</div>
-              <div>{training[0].timeUntil}</div>
-            </div>
-            <div>
+          <div className={styles['training-detail-wrapper']}>
+            <div className={styles['close-icon-container']}>
               <div
-                dangerouslySetInnerHTML={{
-                  __html: training[0].pubContentMarked
-                }}
-              />
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: training[0].privContentMarked
-                }}
-              />
+                className={styles['close-icon']}
+                onClick={this.props.closeTrainingDetail}
+              >
+                <i className={'fas fa-times'} />
+              </div>
             </div>
-            <div>
-              <div>Trainer*innen:</div>
-              <div>{training[0].assignedTrainer1.name}</div>
-              <div>{training[0].assignedTrainer2.name}</div>
-            </div>
+            <div className={styles['training-detail']}>
+              <div className={styles['training-detail--title']}>
+                <h1>{training[0].title}</h1>
+              </div>
+              <div className={styles['training-detail--date']}>
+                <div className={styles['training-detail--date__day']}>
+                  <i className={cx('far fa-calendar-alt', styles['icon'])} />
+                  <Moment format={'DD. MMMM YYYY'} locale={'de-DE'}>
+                    {training[0].date}
+                  </Moment>
+                </div>
+                <div className={styles['training-detail--date__time']}>
+                  <i className={cx('far fa-clock', styles['icon'])} />
 
-            <div>
-              <div>
+                  <div>{training[0].timeFrom}</div>
+                  <div>-</div>
+                  <div>{training[0].timeUntil}</div>
+                </div>
+              </div>
+              <div className={styles['training-detail--location']}>
+                <i className={cx('fas fa-map-marker-alt', styles['icon'])} />
+                <div>
+                  <p className={styles['training-detail--location__title']}>
+                    {training[0].location.title}
+                  </p>
+                  <p>{training[0].location.address1}</p>
+                </div>
+              </div>
+              <hr />
+              <div className={styles['training-detail--content']}>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: training[0].pubContentMarked
+                  }}
+                />
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: training[0].privContentMarked
+                  }}
+                />
+              </div>
+              <div className={styles['training-detail--trainers']}>
+                <div
+                  className={cx(styles['training-detail--trainers__view'], {
+                    [styles['occupied']]:
+                      training[0].assignedTrainer1.id !== 'none' &&
+                      training[0].assignedTrainer1.id !== null
+                  })}
+                >
+                  <div
+                    className={
+                      styles['training-detail--trainers__view--avatar']
+                    }
+                  >
+                    <i class="fas fa-user" />
+                  </div>
+                  <div
+                    className={styles['training-detail--trainers__view--name']}
+                  >
+                    {training[0].assignedTrainer1.id !== 'none' &&
+                    training[0].assignedTrainer1.id !== null ? (
+                      <div>
+                        <p style={{ fontStyle: 'italic' }}>Belegt von:</p>
+                        <p className={styles['trainer-name']}>
+                          {training[0].assignedTrainer1.name}
+                        </p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p style={{ fontStyle: 'italic' }}>Noch nicht belegt</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={cx(styles['training-detail--trainers__view'], {
+                    [styles['occupied']]:
+                      training[0].assignedTrainer2.id !== 'none' &&
+                      training[0].assignedTrainer2.id !== null
+                  })}
+                >
+                  <div
+                    className={
+                      styles['training-detail--trainers__view--avatar']
+                    }
+                  >
+                    <i class="fas fa-user" />
+                  </div>
+                  <div
+                    className={styles['training-detail--trainers__view--name']}
+                  >
+                    {training[0].assignedTrainer2.id !== 'none' &&
+                    training[0].assignedTrainer2.id !== null ? (
+                      <div>
+                        <p style={{ fontStyle: 'italic' }}>Belegt von:</p>
+                        <p className={styles['trainer-name']}>
+                          {training[0].assignedTrainer2.name}
+                        </p>
+                      </div>
+                    ) : (
+                      <div>
+                        <p style={{ fontStyle: 'italic' }}>Noch nicht belegt</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className={styles['training-detail--buttons']}>
                 {training[0].assignedTrainer1.id == undefined ||
                 training[0].assignedTrainer1.id == 'none' ||
                 training[0].assignedTrainer2.id == undefined ||
                 training[0].assignedTrainer2.id == 'none' ? (
-                  <button>Ich bin interessiert</button>
+                  <button className={styles['button-submit']}>
+                    Ich bin interessiert
+                  </button>
                 ) : (
-                  <button disabled={true}>Training bereits augebucht</button>
+                  <button className={styles['button-submit']} disabled={true}>
+                    Training bereits augebucht
+                  </button>
                 )}
-              </div>
-              <div>
-                <button onClick={this.props.closeTrainingDetail}>
-                  Abbrechen
-                </button>
+
+                <div>
+                  <button
+                    className={styles['button-cancel']}
+                    onClick={this.props.closeTrainingDetail}
+                  >
+                    Schließen
+                  </button>
+                </div>
               </div>
             </div>
           </div>
