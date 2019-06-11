9dfb34034e67dfab952c4a075c0ccb6819130e29
thisisgene <emdo2000+genegit@gmail.com>
2019-06-11 14:37:25 +0200

    Added delete user button

--- client/src/components/admin/auth/UserList/UserList.js
+++ client/src/components/admin/auth/UserList/UserList.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { confirmAlert } from 'react-confirm-alert'
+
+import { deleteById } from '../../../../actions/adminActions'
 
 import cx from 'classnames'
 import globalStyles from '../../common/Bootstrap.module.css'
 import styles from './UserList.module.sass'
 
 class UserList extends Component {
+  constructor(props) {
+    super(props)
+  }
+  deleteTeamMember = id => {
+    this.props.deleteById(id, 'trainingTeam')
+    // this.props.history.push('/admin/settings')
+    // console.log(id)
+  }
+
+  confirmDelete = id => {
+    confirmAlert({
+      title: 'Eintrag löschen',
+      message: 'Wollen Sie diesen Eintrag wirklich löschen?',
+      buttons: [
+        {
+          label: 'Löschen',
+          onClick: () => this.deleteTeamMember(id)
+        },
+        {
+          label: 'Abbrechen'
+        }
+      ]
+    })
+  }
   render() {
     const { users } = this.props
     return (
@@ -21,6 +50,7 @@ class UserList extends Component {
               <td>Name</td>
               <td>email</td>
               <td>Sicherheits-Level</td>
+              <td>Löschen</td>
             </tr>
           </thead>
           <tbody>
@@ -30,6 +60,14 @@ class UserList extends Component {
                   <td>{user.name}</td>
                   <td>{user.email}</td>
                   <td>{user.securityLevel}</td>
+                  <td>
+                    <div
+                      className={styles['delete-button']}
+                      onClick={this.confirmDelete.bind(this, user._id)}
+                    >
+                      <i className={'fas fa-minus-circle'} />
+                    </div>
+                  </td>
                 </tr>
               ))}
           </tbody>
@@ -39,4 +77,11 @@ class UserList extends Component {
   }
 }
 
-export default UserList
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(
+  mapStateToProps,
+  { deleteById }
+)(UserList)
