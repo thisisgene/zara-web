34d13c535375ae7a665460857cf4383d53fd6914
thisisgene <emdo2000+genegit@gmail.com>
2019-06-18 12:42:24 +0200

    Added fake english version

--- client/src/components/user/pages/Wissen/Publikationen/zara_pub_data.js
+++ client/src/components/user/pages/Wissen/Publikationen/zara_pub_data.js
@@ -625,7 +625,7 @@ export const pubData = [
 
     de: {
       date: '26. März 2019',
-      title: 'Handbuch für Praktiker*innen: Hate Crime entgegentreten',
+      title: 'Handbuch für Praktiker*innen: <i>Hate Crime</i> entgegentreten',
       shortDescription:
         'Für das Handbuch für Praktiker*innen haben ZARA Expert*innen zusammengefasst, wie hate crimes erkannt werden können, warum es wichtig ist solche zu benennen und wie wir von <i>hate crime</i> Betroffene vom Erstkontakt bis hin zu weiteren (rechtlichen) Schritten unterstützen können.',
       content:
@@ -643,13 +643,23 @@ export const pubData = [
       ]
     },
     en: {
-      date: '26. November 2018',
-      title: '“Know Your Rights”-Broschüren',
+      date: '26. März 2019',
+      title: 'Handbuch für Praktiker*innen: <i>Hate Crime</i> entgegentreten',
       shortDescription:
-        'Für die neuen “Know Your Rights”-Broschüren haben ZARA-Jurist*innen die Rechte von Betroffenen und Zeug*innen von unrechtmäßig erscheinenden Amtshandlungen zusammengefasst. Die Broschüren können Sie sich auch herunterladen und bei Bedarf daheim ausdrucken.',
+        'Für das Handbuch für Praktiker*innen haben ZARA Expert*innen zusammengefasst, wie hate crimes erkannt werden können, warum es wichtig ist solche zu benennen und wie wir von <i>hate crime</i> Betroffene vom Erstkontakt bis hin zu weiteren (rechtlichen) Schritten unterstützen können.',
       content:
-        '<p>Für die neuen "Know Your Rights"-Broschüren haben ZARA-Jurist*innen die Rechte von Betroffenen und Zeug*innen von unrechtmäßig erscheinenden Amtshandlungen zusammengefasst. Einerseits geht es darum, was Betroffene in solchen Momenten tun können, und andererseits darum, wie man als Zeug*in agieren kann, um Betroffenen den Rücken zu stärken.</p><p>Meist werden polizeiliche Amtshandlungen verantwortungsbewusst und korrekt durchgeführt und Beamt*innen leisten durch ihren Einsatz einen wesentlichen Beitrag zu einem friedlichen Miteinander. Die Broschüren sollen in jenen Momenten nützlich sein, wenn das mal nicht so sein sollte.</p><p>Es gibt immer wieder Situationen, in denen den Grund- und Menschenrechten der von einer Amtshandlung betroffenen Personen wenig bis gar keine Bedeutung beigemessen wird. Die ZARA gemeldeten Fälle zeigen auf, dass sich Alltagsrassismus und Vorurteile gegenüber bestimmten Einzelpersonen oder Gruppen, wegen ihrer angenommenen oder tatsächlichen Zugehörigkeit, sowohl in den polizeilichen Strukturen als auch in den Handlungen einzelner Polizist*innen widerspiegeln. Als Betroffene*r oder Zeug*in einer solchen Amtshandlung ist es deshalb umso wichtiger, die eigenen Rechte zu kennen, um diese auch einfordern zu können.</p><br><p>Die Broschüren können Sie sich auch herunterladen und bei Bedarf daheim ausdrucken. Sie finden <a href="https://zara.or.at/_wp/wp-content/uploads/2018/11/faltanleitung_minibooks.pdf">hier</a> auch eine Faltanleitung die anzeigt, wie Sie die Broschüren falten können.</p><p>Der Link zur Broschüre für Betroffene finden Sie hier: <a href="https://zara.or.at/_wp/wp-content/uploads/2018/11/kyr_b_final.pdf">Broschüre Betroffene*r</a></p><p>Der Link zur Broschüre für Zeug*in finden Sie hier: <a href="https://zara.or.at/_wp/wp-content/uploads/2018/11/kyr_z_final.pdf">Broschüre Zeug*in</a></p>',
-      linkText: 'weiterlesen'
+        '<p>Für das Handbuch für Praktiker*innen haben ZARA Expert*innen zusammengefasst, wie hate crimes erkannt werden können, warum es wichtig ist solche zu benennen und wie wir von <i>hate crime</i> Betroffene vom Erstkontakt bis hin zu weiteren (rechtlichen) Schritten unterstützen können. Dieses Handbuch richtet sich an alle Personen, die in der Praxis – z.B. im Opferschutzbereich, in den Bereichen Antidiskriminierung & Menschenrechte, Soziale Arbeit, Flucht & Asyl, etc. – tätig sind. Das Handbuch bietet zudem eine Liste an Organisationen, an die man sich themenspezifisch wenden kann. Das Handbuch steht zum Download zur Verfügung.</p>',
+      linkText: 'weiterlesen',
+      download: [
+        {
+          fileLocation:
+            'pdf/Handbuch fuer Praktikerinnen_hate crime entgegentreten.pdf',
+          fileName:
+            'Handbuch fuer Praktikerinnen_hate crime entgegentreten.pdf',
+
+          fileSize: '913 KB'
+        }
+      ]
     }
   }
 ]
