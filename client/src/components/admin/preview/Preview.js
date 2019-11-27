7dd5237d0475cc322ee16de5209afb839f22886f
thisisgene <emdo2000+genegit@gmail.com>
2019-11-27 16:12:35 +0100

    Added iframe to preview

--- client/src/components/admin/preview/Preview.js
+++ client/src/components/admin/preview/Preview.js
@@ -16,12 +16,42 @@ class Preview extends Component {
     super(props)
     this.state = {
       lang: 'de',
-      newsId: props.match.params.newsId
+      newsId: props.match.params.newsId,
+      hasIframe: false,
+      newDescription: ''
     }
   }
   componentDidMount() {
     this.props.getById(this.props.match.params.newsId, 'news')
     console.log(this.props)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.news != this.props.news && this.props.news.newsItem) {
+      let description = this.props.news.newsItem[this.state.lang].description
+      if (description.includes(`&lt;iframe`)) {
+        let iframeContent = description.substring(
+          description.lastIndexOf('&lt;iframe'),
+          description.lastIndexOf('iframe&gt;') + 10
+        )
+        let iframe = document.createElement('div')
+        iframe.innerHTML = iframeContent
+        let start = description.indexOf('&lt;iframe')
+        let end = description.indexOf('iframe&gt;') + 'iframe&gt;'.length
+        let newDescription = description.replace(
+          description.substring(start, end),
+          iframe.textContent
+        )
+
+        this.setState(
+          {
+            hasIframe: true,
+            newDescription: newDescription
+          },
+          () => console.log(this.state.newDescription)
+        )
+      }
+    }
   }
 
   onClick = e => {
@@ -73,10 +103,21 @@ class Preview extends Component {
             <div className={styles['news-detail']}>
               <HeroUnit data={newsItem} lang={lang} />
               <div className={styles['fakeOneLineAlert']} />
-              <div
-                className={styles['news-detail--text']}
-                dangerouslySetInnerHTML={{ __html: newsItem[lang].description }}
-              />
+              {this.state.hasIframe ? (
+                <div
+                  className={styles['news-detail--text']}
+                  dangerouslySetInnerHTML={{
+                    __html: this.state.newDescription
+                  }}
+                />
+              ) : (
+                <div
+                  className={styles['news-detail--text']}
+                  dangerouslySetInnerHTML={{
+                    __html: newsItem[lang].description
+                  }}
+                />
+              )}
               {newsItem.videos &&
                 newsItem.videos
                   .filter(video => video !== null)
@@ -124,7 +165,4 @@ const mapStateToProps = state => ({
   news: state.news
 })
 
-export default connect(
-  mapStateToProps,
-  { getById }
-)(Preview)
+export default connect(mapStateToProps, { getById })(Preview)
