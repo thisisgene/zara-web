a56db4e1962189685840772ec2559269929cdb8a
thisisgene <emdo2000+genegit@gmail.com>
2020-06-29 17:29:56 +0200

    added rr19 EN

--- client/src/components/user/pages/Wissen/Publikationen/rassismusReport_data.js
+++ client/src/components/user/pages/Wissen/Publikationen/rassismusReport_data.js
@@ -35,16 +35,16 @@ export const reportGridData = [
     image: 'zara_rr/covers/Deckblatt_RR2019.jpg',
     links: [
       { link: 'pdf/ZARA-Rassismus_Report_2019.pdf', linkText: 'deutsch' },
-      // { link: 'pdf/ZARA-Rassismus_Report_2019_EN.pdf', linkText: 'englisch' }
+      { link: 'pdf/ZARA-Rassismus_Report_2019_EN.pdf', linkText: 'englisch' }
     ],
     de: {
       title: 'RR2019',
       text: ''
     },
-    // en: {
-    //   title: 'RR2019',
-    //   text: ''
-    // }
+    en: {
+      title: 'RR2019',
+      text: ''
+    }
   },
   {
     id: 'rr2018',
