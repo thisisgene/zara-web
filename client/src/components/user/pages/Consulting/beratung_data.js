c5535adbd3c0a90f75e9bd357d055f70846444d5
thisisgene <emdo2000+genegit@gmail.com>
2021-01-18 17:58:13 +0100

    Changed style

--- client/src/components/user/pages/Consulting/beratung_data.js
+++ client/src/components/user/pages/Consulting/beratung_data.js
@@ -421,9 +421,9 @@ export const wasSieTunKoennenHeroData = {
       'Melden Sie rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing an die ZARA Beratungsstellen. Bei Bedarf stehen Ihnen die psychosozial und juristisch geschulten Berater*innen gerne kostenlos zur Seite. Sowohl online als auch offline ist Eingreifen gefragt. Durch zivilcouragiertes Handeln stärken Sie Betroffenen den Rücken.'
   },
   en: {
-    title: 'Werden Sie aktiv',
+    title: 'Get involved',
     text:
-      'Melden Sie rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing an die ZARA Beratungsstellen. Bei Bedarf stehen Ihnen die psychosozial und juristisch geschulten Berater*innen gerne kostenlos zur Seite. Sowohl online als auch offline ist Eingreifen gefragt. Durch zivilcouragiertes Handeln stärken Sie Betroffenen den Rücken.'
+      'Report racist discrimination, online hate as well as cyberbullying to the ZARA team. If necessary, our psychosocially and legally trained advisors will be happy to assist you free of charge. We need to stand up against discrimination online and offline. By showing civil courage, we strengthen those around us.'
   }
 }
 
@@ -455,19 +455,19 @@ export const wasSieTunKoennenInfoBox1 = {
     cards: [
       {
         color: 'default',
-        title: 'MELDEN',
+        title: 'REPORT AN INCIDENT',
         text:
-          'Melden Sie rassistische Diskriminierung - on- sowie offline - und Hass im Netz an ZARA. ZARA dokumentiert alle Meldungen in einer eigen dafür vorgesehen Falldatenbank, welche wiederum für Berichte und Analysen anonymisiert aufgearbeitet werden. Diese Berichte sind eine wichtige qualitative Quelle, um auf das Ausmaß von Rassismus sowie Hass im Netz hinzuweisen und sichtbar zu machen. So ist ZARA in der Lage, Problematiken bzw. Tendenzen zu erkennen und mit Aktivitäten, Initiativen und Kampagnen dagegen vorzugehen.',
+          'Report racist discrimination - online and offline – as well as online hate to ZARA. ZARA documents all cases in our database, which in turn is processed anonymously for written reports and analyses. These are an important qualitative source for pointing out the extent of racism and online hate in Austria – making them visible to a greater public. This way, we can observe social issues and/or new developments, which we can then address with targeted initiatives and campaigns.',
         link: 'beratung/melden',
-        linkText: 'Melden'
+        linkText: 'Report an incident'
       },
       {
         color: 'default',
-        title: 'BERATUNGSTERMIN VEREINBAREN',
+        title: 'ARRANGE AN APPOINTMENT',
         text:
-          'Wenn Sie selbst von Rassismus oder Hass im Netz betroffen sind, können Sie sich kostenlos von uns beraten lassen. Die ZARA Beratungsstellen können in solchen Situationen unterstützen! Sie erhalten kostenlos und zeitnah Informationen zu möglichen (rechtlichen) Schritten und erfahren, wie Sie selbst aktiv werden können. Die Mitarbeiter*innen können Sie bei der Durchführung der rechtlichen aber auch anderen möglichen Schritte unterstützen. Wenn Sie eine persönliche Beratung in Anspruch nehmen möchten, vereinbaren Sie bitte telefonisch oder per verschlüsseltem E-Mail einen Termin mit uns.',
+          'Our team can provide support if you are affected by racism or online hate! You will receive free and timely information on possible (legal) steps and learn how you can get involved. If you would like to take advantage of a personal consultation, please make an appointment with us by phone or (encrypted) email.',
         link: 'beratung',
-        linkText: 'Beratungstermin vereinbaren'
+        linkText: 'Arrange an appointment'
       }
     ]
   }
@@ -501,20 +501,20 @@ export const wasSieTunKoennenInfoBox2 = {
     cards: [
       {
         color: 'default',
-        title: 'ZIVILCOURAGE ZEIGEN',
+        title: 'SHOW CIVIL COURAGE',
         text:
-          'Bei rassistischer Diskriminierung sowie bei Hass im Netz ist Eingreifen gefragt! Zivilcourage heißt in “unangenehmen” Situationen in der Öffentlichkeit - on- & offline - einzugreifen. Eingreifen kann bedeuten, dass man nicht weg sieht, Betroffenen Unterstützung anbietet, Vorfälle dokumentiert, Unterstützung holt, und vieles mehr. Auch in der online Welt ist zivilcouragiertes Handeln gefragt, um Hass im Netz in Grenzen zu halten, Räume nicht stillschweigend aufzugeben und wiederum Betroffenen den Rücken zu stärken. Es erfordert Mut! Wichtig ist dabei stets die eigenen Grenzen und Fähigkeiten sowie den Wunsch der Betroffenen zu erkennen und danach zu handeln.',
+          'Whether its racist discrimination or online hate, we need to get involved! Civil courage means to intervene in “unpleasant” situations in public - online and offline. Intervening can mean not looking the other way, offering support to those affected, documenting incidents, getting support, and much more. Online civil courage is also required to limit online hate, to keep online spaces open and, in turn, to support those affected. It takes courage to stand up against discrimination! It is important to recognize your own limits and abilities as well as the wishes of those affected and to act accordingly.',
         link: 'mitmischen',
-        linkText: 'Mitmischen'
+        linkText: 'Get involved'
       },
       {
         color: 'default',
-        title: 'WEITERSAGEN',
+        title: 'SPREAD THE WORD',
         text:
-          'Zu einer Sensibilisierung und Aufklärung der Gesellschaft über rassistische Diskriminierung bedarf es öffentlicher Diskurse. Auch bei Hass im Netz ist das Wissen, dass es sich dabei oft um strafrechtlich relevante Tatbestände handelt, nach wie vor nicht bei allen angekommen. Weil Rassismus und Hass im Netz am besten durch Aufklärungsarbeit bekämpft werden können, freuen wir uns, wenn Sie uns dabei unterstützen, unser Beratungs- und Trainingsangebot an so viele Personen wie möglich weiterzuleiten.',
+          'In order to address the issue of racism, it is important to raise public awareness. Few people are aware that online hate can be judged a criminal offence. Racism and online hate are best addressed through educational work. You can support our work by spreading the word about our advice and training services.',
         linkPath: 'external',
         link: 'mitmischen',
-        linkText: 'Mach auf unser Beratungsangebot aufmerksam'
+        linkText: 'Raise awareness'
       }
     ]
   }
@@ -555,21 +555,21 @@ export const wasSieTunKoennenCardGridObject = {
         size: 'default',
         type: 'imageCard',
         image: 'icons/beratung.png',
-        title: 'Was Wir Dann Tun',
+        title: 'What we can do',
         link: 'beratung/waswirdanntun'
       },
       {
         size: 'default',
         type: 'imageCard',
         image: 'icons/mitmischen.png',
-        text: 'Mitmischen',
+        text: 'Get involved',
         link: 'mitmischen'
       },
       {
         size: 'default',
         type: 'imageCard',
         image: 'icons/wissen.png',
-        text: 'Wissen',
+        text: 'Resources',
         link: 'wissen'
       }
     ]
@@ -577,13 +577,14 @@ export const wasSieTunKoennenCardGridObject = {
 }
 
 export const wasWirTunKoennenHeroData = {
+  class: 'no-image',
   de: {
     title: 'Was Wir dann tun'
     // text:
     //   'Die ZARA Beratungsstellen dokumentieren rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing und ergreifen weitere Maßnahmen. Melden Sie rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing. Bei Bedarf stehen Ihnen die psychosozial und juristisch geschulten Berater*innen gerne kostenlos zur Seite. Sowohl online als auch offline ist Eingreifen gefragt. Durch zivilcouragiertes Handeln wird Betroffenen der Rücken gestärkt und unterschiedliche Räume können verteidigt und zurückgewonnen werden.'
   },
   en: {
-    title: 'Was Wir dann tun'
+    title: 'What we can do'
     // text:
     //   'Die ZARA Beratungsstellen dokumentieren rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing und ergreifen weitere Maßnahmen. Melden Sie rassistische Diskriminierung, Online Hass und Hetze sowie Cybermobbing. Bei Bedarf stehen Ihnen die psychosozial und juristisch geschulten Berater*innen gerne kostenlos zur Seite. Sowohl online als auch offline ist Eingreifen gefragt. Durch zivilcouragiertes Handeln wird Betroffenen der Rücken gestärkt und unterschiedliche Räume können verteidigt und zurückgewonnen werden.'
   }
@@ -620,23 +621,23 @@ export const wasWirTunKoennenInfoBox1 = {
     cards: [
       {
         color: 'default',
-        title: 'BERATUNG',
+        title: 'SUPPORT',
         text:
-          'Die Mitarbeiter*innen der Beratungsstellen beraten Sie kostenlos und vertraulich bei Vorfällen von Hass im Netz sowie bei rassistischer Diskriminierung. Bei einer Beratung beschreiben Sie den Vorfall und besprechen gemeinsam mit der Berater*in mögliche weitere Schritte. Die Beratungen können persönlich, telefonisch, per Chat oder per E-Mail stattfinden. Sie können dabei gerne anonym bleiben, wenn Sie das möchten. Für persönliche Beratungen vereinbaren Sie bitte telefonisch einen Termin. Das Beratungsteam bemüht sich, Ihre Anfrage möglichst rasch zu bearbeiten - je nach Anzahl der Meldungen kann es manchmal länger dauern. Wenn Sie kein Deutsch sprechen, versuchen wir die Beratung mit Unterstützung eines*r Dolmetscher*in durchzuführen.',
+          'If you experience racist discrimination, online hate or cyberbullying, our team will advise you confidentially and free of charge. During a consultation, you can describe the incident and discuss possible steps. The consultations can take place face-to-face, by telephone, by chat or by email. If you wish, you can decide to keep your identity anonymous. For a face-to-face meeting, please make an appointment by phone. Our team will then process your request as quickly as possible. Depending on the number of cases we receive, it can sometimes take longer for us to get back to you. If you do not speak German, we will try to arrange for an interpreter.',
         link: 'beratung',
-        linkText: 'Zur Beratung'
+        linkText: 'Get support'
       },
       {
         color: 'default',
-        title: 'RECHTSAUSKUNFT',
+        title: 'LEGAL INFORMATION',
         text:
-          'Bei rechtlichen Fragen zu den Themen rassistischer Diskriminierung sowie Hass im Netz stehen Ihnen die Mitarbeiter*innen der Beratungsstelle persönlich, telefonisch, per Email oder Chat zur Verfügung. Wichtige Gesetze sind beispielsweise Bestimmungen aus dem Strafgesetzbuch (StGB) sowie das Gleichbehandlungsgesetz (GlBG) oder das Verbotsgesetz (VerbotsG).'
+          'If you have any legal questions regarding racist discrimination and online hate, you can reach our team by phone, encrypetd email or chat. Relevant laws regarding racist discrimination and online hate in Austria include, for example, provisions from the Criminal Code (Strafgesetzbuch - StGB) as well as the Equal Treatment Act (Gleichbehandlungsgesetz - GlBG) or the Prohibition Act (Verbotsgesetz  - VerbotsG).'
       },
       {
         color: 'default',
-        title: 'BEGLEITUNG ZU GERICHT/BEHÖRDEN',
+        title: 'ASSISTING WITH COURTS / AUTHORITIES',
         text:
-          'Gerne begleiten Sie Mitarbeiter*innen der Beratungsstelle zu Terminen bei der Polizei, bei Gericht oder bei sonstigen Behörden, wenn es um rassistische Diskriminierung oder um Hass im Netz geht. Als Vertrauensperson kann der*die Berater*in Sie gut unterstützen und mit Ihnen danach weitere Schritte besprechen.'
+          'Our team can accompany you to your appointments with the police, court or other authorities when it comes to racist discrimination or online hate. As a person of trust, our counsellors will be able to support you and then discuss further steps with you.'
       }
     ]
   }
@@ -673,27 +674,27 @@ export const wasWirTunKoennenInfoBox2 = {
     cards: [
       {
         color: 'default',
-        title: 'ENTFERNUNG BEANTRAGEN',
+        title: 'REQUEST REMOVAL',
         text:
-          'Melden Sie der Beratungsstelle Hasspostings oder rassistische Beschmierungen! Die Mitarbeiter*innen überprüfen, ob diese gegen das Gesetz verstoßen und bemühen sich um eine rasche Entfernung.',
+          'Report hate postings or racist smearings to us! We will check whether these violate the law and work to remove them quickly.',
         link: 'beratung',
-        linkText: 'Zur Beratung'
+        linkText: 'Get support'
       },
       {
         color: 'default',
-        title: 'UNTERSTÜTZUNG BEI ANZEIGEN',
+        title: 'SUPPORT WITH FILING A COMPLAINT',
         text:
-          'Bei Fällen von rassistischer Diskriminierung oder Hass im Netz, die gegen ein Gesetz verstoßen, bespricht Ihr*e Berater*in mit Ihnen die bestehenden Handlungsmöglichkeiten. Wenn Sie eine Anzeige einbringen möchten und diese sinnvoll erscheint, unterstützen wir Sie gerne dabei.'
+          'Your counsellor can discuss cases of racist discrimination or online hate with you in order to examine all the options available to you. We will be happy to support you in filing a complaint in case of a violation of Austrian law (and if doing so is feasible).'
       },
       {
         color: 'default',
-        title: 'RECHTSHILFEFOND HASS IM NETZ',
+        title: 'LEGAL AID FUND AGAINST ONLINE HATE',
         text:
-          'Gemeinsam mit Sigrid Maurer hat der Verein ZARA im Oktober 2018 einen Rechtshilfefonds eingerichtet, mit dem zivilrechtliche Klagen bei Fällen von Hass im Netz finanziert werden können. Sind Sie von Hass im Netz betroffen wenden Sie sich an ein*e Berater*in, der*die mit Ihnen bespricht, ob es möglich ist, eine Klage einzubringen und ob die Kosten vom Rechtshilfefonds übernommen werden können.',
+          'Together with Sigrid Maurer, ZARA set up a legal aid fund in October 2018 that can be used to finance civil lawsuits in cases of online hate. If you are affected by online hate, please contact us. We will then discuss with you whether it is possible to file a lawsuit and whether the legal aid fund can cover the costs.',
         link:
           'https://www.respekt.net/projekte-unterstuetzen/details/projekt/1780/',
         linkPath: 'external',
-        linkText: 'Näheres zum Rechtshilfefonds'
+        linkText: 'More information'
       }
     ]
   }
@@ -734,21 +735,21 @@ export const wasWirTunKoennenCardGridObject = {
         size: 'default',
         type: 'imageCard',
         image: 'icons/beratung.png',
-        title: 'Was Sie Tun Können',
+        title: 'What you can do',
         link: 'beratung/wassietunkoennen'
       },
       {
         size: 'default',
         type: 'imageCard',
         image: 'icons/mitmischen.png',
-        title: 'Mitmischen',
+        title: 'Get involved',
         link: 'mitmischen'
       },
       {
         size: 'default',
         type: 'imageCard',
         image: 'icons/wissen.png',
-        title: 'Wissen',
+        title: 'Resources',
         link: 'wissen'
       }
     ]
