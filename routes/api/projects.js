8822e2820060aef1ca39b3341234afd91e0924ad
thisisgene <emdo2000+genegit@gmail.com>
2021-05-02 21:57:12 +0200

    Changed email address

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -423,7 +423,7 @@ sendPresseclubEmail = report => {
   // setup email data with unicode symbols
   let mailOptions = {
     from: '"ZARA Server" <serpig.testuser@gmail.com>', // sender address
-    to: 'emdo2000@gmail.com', // list of receivers //beratung@zara.or.at
+    to: 'office@concordia.at', // list of receivers //beratung@zara.or.at
     subject: 'Neue Meldung eingegangen', // Subject line
     text: outputPlain, // plain text body
     html: outputHtml, // html body
